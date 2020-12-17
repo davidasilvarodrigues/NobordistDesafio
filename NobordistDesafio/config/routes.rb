cc44e23346762021a2a742834800d9451c724232
Davi Rodrigues <davisr@id.uff.br>
2020-12-17 17:01:28 -0300

    Finalizando api e implementando seeds

--- NobordistDesafio/config/routes.rb
+++ NobordistDesafio/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :veiculos
+    end
+  end # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlendRails.application.routes.draw do namespace :api do  namespace :v1 do   resources :articles  end end# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
 end
